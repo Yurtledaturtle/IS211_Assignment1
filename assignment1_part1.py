d376dee1bd6f1e0a65f709c1c6e4bb2a333f48d2
Yurtledaturtle <mkoski6@gmail.com>
2021-09-17 13:45:17 -0500

    commit changes to assign1

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,32 @@
 
-def listDivide(numbers, divide):
+def listDivide(numbers, divide=2):
+    count_ = 0
+    for i in numbers:
+        if i % divide == 0:
+            count_ += 1
+    return count_
+
     """
-    The function returns the number of elements in the numbers list that are divisibleby divide
+    This function returns the number of elements in the list that are divisible by divide
     """
+
+    class ListDivideException:
+        pass
+
     pass
+
 
 def testListDivide():
     """
     Test listDivide
     """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
+    assert listDivide([1, 2, 3, 4, 5]) == 2
+    assert listDivide([2, 4, 6, 8, 10]) == 5
+    assert listDivide([30, 54, 63, 98, 100], divide=10) == 2
     assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
-    
+    assert listDivide([1, 2, 3, 4, 5], 1) == 5
+
+
 if __name__ == "__main__":
+    print("All test passed")
     testListDivide()
